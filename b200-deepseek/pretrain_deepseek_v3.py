935546f4c9c7cf6abf210df2b5b461047262883f
rick chen <admin@rickruguichen.altostrat.com>
2025-09-20 23:41:27 +0000

    update configs?

--- b200-deepseek/pretrain_deepseek_v3.py
+++ b200-deepseek/pretrain_deepseek_v3.py
@@ -144,7 +144,7 @@ def override_recipe_configs(
         recompute_layers=recompute_layers,
         activation_offload_layers=activation_offload_layers,
         compute_dtype="fp8",
-        fp8_recipe="mxfp8",
+        fp8_recipe="ds",
         recompute_modules=recompute_modules,
         use_te_act_func=None,
         act_func_fp8_input_store=None,
