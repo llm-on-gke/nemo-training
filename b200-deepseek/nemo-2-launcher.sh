82f2f4ad1caac2b4bf06dfcd839e0c62141e2c4a
rick chen <admin@rickruguichen.altostrat.com>
2025-09-26 04:27:40 +0000

    tweak parameter

--- b200-deepseek/nemo-2-launcher.sh
+++ b200-deepseek/nemo-2-launcher.sh
@@ -28,7 +28,7 @@ git checkout r2.4.0
 pip install '.[all]'
 #pip install megatron-core@git+https://github.com/NVIDIA/Megatron-LM.git@core_r0.13.0
 cd /workspace
-git clone https://github.com/Nvidia/Megatron-LM//Megatron-LM/
+git clone https://github.com/Nvidia/Megatron-LM/
 cd Megatron-LM
 git checkout core_r0.13.0 
 pip install -e .
